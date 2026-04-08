fc48794fd3cb9d2c9fa5a9486abcb25bd7c38584
TerraBreus <studentbreus@gmail.com>
2026-04-08 11:23:05 +0200

    making ex00 interactive

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,8 +4,57 @@
 #include "Cat.hpp"
 #include <iostream>
 #include <string>
+#include <unistd.h>
+// These should be in a header called colors.hpp"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define PURPLE "\033[35m"
+#define RESET "\033[m"
 
-int	main(void)
+void	sleepyPrinter(std::string prompt, std::string color)
+{
+	std::cout << color;
+	std::cout << prompt << std::endl;
+	std::cout << RESET;
+	sleep(1);
+}
+
+std::string askForInput(void)
+{
+	std::string	userInput;
+
+	while (true)
+	{
+		std::getline(std::cin, userInput);
+		if (std::cin.eof())
+		{
+			std::cout << "Exiting program..." << std::endl;
+			exit (0);
+		}
+		if (userInput.empty())
+			continue ;
+		else
+			break;
+	}
+	return (userInput);
+}
+
+std::string	startMessage(void)
+{
+	std::cout << PURPLE;
+	std::cout << "Welcome to my interactive program teaching you about Polymorphism." << std::endl;
+	std::cout << "What would you like to see first?\n\n" << std::endl;
+	std::cout << GREEN;
+	std::cout << "1. Correct use of Polymorphism (Cats and Dogs derived from the Animal class)\n";
+	std::cout << RED;
+	std::cout << "2. Incorrect use of Polymorphism (WrongAnimal class)." << std::endl;
+	std::cout << RESET;
+
+	std::string	result = askForInput();
+	return (result);
+}
+
+void	correctPolymorphism(void)
 {
 	const Animal*	meta = new Animal();
 	const Animal*	j = new Dog();
@@ -20,13 +69,34 @@ int	main(void)
 	delete	meta;
 	delete	j;
 	delete	i;
-	
-	std::cout << std::endl;
-	std::cout << "Now we do the wrong classes." << std::endl;
+}
 
+void	incorrectPolymorphism(void)
+{
 	const WrongAnimal*	wrong = new WrongCat();
 	std::cout << wrong->getType() << std::endl;
 	wrong->makeSound();
 	delete	wrong;
+}
+
+void	idiotBehavior(void)
+{
+	sleepyPrinter("Oh well...", RED);
+	sleepyPrinter("Guess we can start the program again", RED);
+}
+
+int	main(void)
+{
+	std::string	userInput;
+
+	userInput = startMessage();
+	sleep(1);
+	if (userInput == "1")
+		correctPolymorphism();
+	else if (userInput == "2")
+		incorrectPolymorphism();
+	else
+		idiotBehavior();
+
 	return (0);
 }
