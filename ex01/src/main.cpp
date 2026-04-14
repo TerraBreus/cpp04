4468c268c9509f2c35570e76c172b73cf7a66dd8
TerraBreus <studentbreus@gmail.com>
2026-04-14 17:27:09 +0200

    refactoring functions in main for readability

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,35 +6,75 @@
 #include <string>
 #include <unistd.h>
 #include "../inc/terminal_colors.h"
-#include "../inc/utils.hpp"
 
-int	main(void)
+void	waitForUser(std::string message)
 {
-	
-	Animal*	catsAndDogs[100];
+	std::cout << "\n" << message << std::endl;
+	std::cout << "press ENTER to continue..." << std::endl;
+	std::cin.get();
+}
 
+static void	allocateCatsAndDogs(Animal* catsAndDogs[100])
+{
 	for (int i = 0; i < 100; i++)
 	{
+		std::cout << i << std::endl;
 		if (i < 50)
 			catsAndDogs[i] = new Cat;
 		else
 			catsAndDogs[i] = new Dog;
 	}
 
+}
+
+static void	destroyCatsAndDogs(Animal* catsAndDogs[100])
+{
 	for (int i = 0; i < 100; i++)
 	{
 		std::cout << i << std::endl;
 		delete catsAndDogs[i];
 	}
+}
+
+static void	deepCopyExample(void)
+{
+	std::cout << "Code that is about to be executed:" << std::endl;
+	std::cout << PURPLE;
+	std::cout << "Cat	one, two;\n";
+	std::cout << "one.createThought(\"bones\", 0);\n";
+	std::cout << "two = one;\n";
+	std::cout << "one.createThought(\"apples\", 0);" << RESET << std::endl;
+	waitForUser("");
 
 	Cat	one, two;
 	one.createThought("bones", 0);
 	two = one;
 	one.createThought("apples", 0);
+
+	std::cout << YELLOW;
+	std::cout << "Cat Two now has a (deep) copy of Cat One\n";
+	std::cout << "If we change anything for Cat One, Cat Two should stay the same\n";
+	std::cout << "We can check this with the `Cat.thinkThought()` function." << std::endl;
+	std::cout << RESET;
+	waitForUser("");
+
 	std::cout << "Two thinks: ";
 	two.thinkThought(0);
 	std::cout << "One thinks: ";
 	one.thinkThought(0);
+}
+
+int	main(void)
+{
+	Animal*	catsAndDogs[100];
+
+	waitForUser("= = ASSIGNING 50 DOGS AND 50 CATS = =");
+	allocateCatsAndDogs(catsAndDogs);
+	waitForUser(" = = DESTROYING CATS AND DOGS AS ANIMALS = =");
+	destroyCatsAndDogs(catsAndDogs);
+	waitForUser("= = DEEP COPY EXAMPLE = =");
+	deepCopyExample();
+	waitForUser("= = SECOND DEEP COPY EXAMPLE = =");
 
 	return (0);
 }
