c98e48f42ff4fcd8e7685f60aa28719b4969d07a
TerraBreus <studentbreus@gmail.com>
2026-04-20 13:06:08 +0200

    fix: add correct colors to WrongAnimal class messages

--- ex00/src/WrongAnimal.cpp
+++ ex00/src/WrongAnimal.cpp
@@ -1,10 +1,13 @@
 #include "../inc/WrongAnimal.hpp"
 #include <iostream>
+#include "../inc/terminal_colors.h"
 
 WrongAnimal::WrongAnimal(void) 
 {
 	this->type = "no type set";
+	std::cout << BG_BLACK;
 	std::cout << "An WrongAnimal class has been created." << std::endl;
+	std::cout << RESET;
 }
 
 WrongAnimal::WrongAnimal(const WrongAnimal& other)
@@ -23,12 +26,16 @@ WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other)
 
 WrongAnimal::~WrongAnimal(void) 
 {
+	std::cout << BG_BLACK;
 	std::cout << "An WrongAnimal class has been destroyed." << std::endl;
+	std::cout << RESET;
 }
 
 void	WrongAnimal::makeSound(void) const
 {
+	std::cout << BG_BLACK;
 	std::cout << "*GENERIC ANIMAL SOUND*" << std::endl;
+	std::cout << RESET;
 }
 
 std::string	WrongAnimal::getType(void) const
--- ex00/src/WrongCat.cpp
+++ ex00/src/WrongCat.cpp
@@ -1,10 +1,13 @@
 #include "../inc/WrongCat.hpp"
+#include "../inc/terminal_colors.h"
 #include <iostream>
 
 WrongCat::WrongCat(void)
 {
 	this->type = "WrongCat";
+	std::cout << BG_BLACK;
 	std::cout << "A WrongCat has been created." << std::endl;
+	std::cout << RESET;
 }
 
 WrongCat::WrongCat(const WrongCat& other)
@@ -23,10 +26,14 @@ WrongCat& WrongCat::operator=(const WrongCat& other)
 
 WrongCat::~WrongCat(void)
 {
+	std::cout << BG_BLACK;
 	std::cout << "A WrongCat has been annihilated." << std::endl;
+	std::cout << RESET;
 }
 
 void	WrongCat::makeSound(void) const
 {
+	std::cout << BG_BLACK;
 	std::cout << "bork bork amirite guys?" << std::endl;
+	std::cout << RESET;
 }
