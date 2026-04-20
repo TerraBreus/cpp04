75534bde7e3805294cc6d0ce0e55aa0269bbddb3
TerraBreus <studentbreus@gmail.com>
2026-04-20 13:07:32 +0200

    chore: update function prototypes in utils.hpp

--- ex00/inc/utils.hpp
+++ ex00/inc/utils.hpp
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "../inc/Animal.hpp"
 #include <iostream>
 #include "terminal_colors.h"
 #define NEWLINE 1
@@ -9,9 +10,9 @@
 void	sleepyPrinter(std::string prompt, std::string color);
 void	idiotBehavior(void);
 void	sleepyPrinter(std::string prompt);
-std::string askForInput(int	cinOrStr);
+std::string askForInput(int	cinOrStr, const Animal* ptr[3]);
 void	clearTerminal(void);
 void	loadingScreen(std::string prompt);
 void	printer(std::string prompt, std::string color, int newline);
 void	printer(std::string prompt, int newline);
-void	step();
+void	step(const Animal* ptr[3]);
